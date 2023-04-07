f0103224ebacd36f9ba689c02babad095d714e6f
sahilsgovekar <sahilsgovekar@gmail.com>
2023-04-07 11:30:30 +0530

    compleated blog page and its component

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 #table creation
 class Blog(db.Model):
     __tablename__ = "blogs"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    content = db.Column(db.String(100000000))
+    category = db.Column(db.String(50))
+    content = db.Column(db.Text)
 
 
 @app.route("/sahil-govekar")
 def home():
-    return render_template("index.html")
+    posts = db.session.query(Blog).all()
+    return render_template("index.html", posts=posts)
 
 @app.route("/")
 def hom():
@@ -27,12 +30,20 @@ def hom():
 
 @app.route("/blog")
 def blog():
-    return render_template("blog.html")
+    posts = db.session.query(Blog).all()
+    return render_template("blog.html", posts=posts)
 
 
 # #rendering blog based on title
-# @app.route("/blog/<int:index>")
-# def show_post(index):5
+@app.route("/blog/<int:index>")
+def show_post(index):
+    requested_post = None
+    posts = db.session.query(Blog).all()
+    for blog_post in posts:
+        if blog_post.id == index:
+            requested_post = blog_post
+
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
